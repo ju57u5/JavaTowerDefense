090fe79666ef62e6c37abc1ce15ec3294c2f5a65
Crexdor <olafvk@web.de>
2015-05-29 23:00:04 +0200

    Cha0s and Admin added

--- src/tk/ju57u5v/javatowerdefense/tower/Admin.java
+++ src/tk/ju57u5v/javatowerdefense/tower/Admin.java
@@ -1,9 +1,9 @@
 package tk.ju57u5v.javatowerdefense.tower;
 
-public class Mod extends Tower {
+public class Admin extends Tower {
 	
-	public Mod(){
-		damage = 1;
+	public Admin(){
+		damage = 2;
 		attack_speed = 1;
 		price = 100;
 	}
