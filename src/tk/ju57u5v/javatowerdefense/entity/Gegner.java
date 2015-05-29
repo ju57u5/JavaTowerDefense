be9987699fb66b82ba7fa012c98334abb0ee7c28
Crexdor <olafvk@web.de>
2015-05-29 23:04:38 +0200

    Added Moneydroped variable

--- src/tk/ju57u5v/javatowerdefense/entity/Gegner.java
+++ src/tk/ju57u5v/javatowerdefense/entity/Gegner.java
@@ -9,7 +9,7 @@ import tk.ju57u5v.engine.graphics.Sprite;
 public abstract class Gegner extends Entity {
 
 	protected Sprite texture;
-	protected double speed,leben,damage;
+	protected double speed,life,damage,exp,drops;
 	private boolean started = false;
 	private Vec2 startPos = new Vec2();
 	
