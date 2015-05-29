44bb9d0adbb52fe557d6be40606d8500748c48a6
Crexdor <olafvk@web.de>
2015-05-29 22:42:56 +0200

    Mod added

--- src/tk/ju57u5v/javatowerdefense/tower/Tower.java
+++ src/tk/ju57u5v/javatowerdefense/tower/Tower.java
@@ -4,7 +4,7 @@ import tk.ju57u5v.engine.graphics.Sprite;
 
 public class Tower {
 	
-	protected double damage,attck_speed,price;
+	protected double damage,attack_speed,price;
 	protected Sprite texture;
 	
 	public Tower() {
