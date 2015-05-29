f5ca6029fb0e08393a33b5aac621217ade316421
Crexdor <olafvk@web.de>
2015-05-29 23:12:47 +0200

    Habs gelöscht bitte

--- src/tk/ju57u5v/javatowerdefense/tower/Tower.java
+++ src/tk/ju57u5v/javatowerdefense/tower/Tower.java
@@ -1,11 +1,14 @@
 package tk.ju57u5v.javatowerdefense.tower;
 
+
+import tk.ju57u5v.engine.components.Vec2;
 import tk.ju57u5v.engine.graphics.Sprite;
 
-public class Tower {
+public abstract class Tower{
 	
 	protected double damage,attack_speed,price;
 	protected Sprite texture;
+	private Vec2 Pos = new Vec2();
 	
 	public Tower() {
 		super();
